2be0ae45de43b8574a04ff2120bf78f257483cb9
Pedro Leonardo <pedroleon.pc@hotmail.com>
2023-08-21 09:13:57 -0300

    update in cors

--- travel-backend/src/app.js
+++ travel-backend/src/app.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const router = require('./router')
+const router = require('./router');
 
 const app = express();
 
 app.use(router);
-
 
 module.exports = app;
--- travel-backend/src/server.js
+++ travel-backend/src/server.js
@@ -1,8 +1,10 @@
 const app = require('./app');
 require('dotenv').config();
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3333;
 
+app.use(cors());
 app.listen(PORT, ()=> 
     console.log('Server Runing...')
 );
